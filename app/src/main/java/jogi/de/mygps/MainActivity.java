b9a053ce3bc2afff898c2a4f9156b8cc33a26476
Jochen Weiss <jochen.weiss@continental-corporation.com>
2018-12-12 09:17:56 +0100

    Changed activity_main.xml to keep the display always on

--- app/src/main/java/jogi/de/mygps/MainActivity.java
+++ app/src/main/java/jogi/de/mygps/MainActivity.java
@@ -1,27 +1,13 @@
 package jogi.de.mygps;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity{
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LocationListenerImpl ll = new LocationListenerImpl(this);
         }
-
-
    }
