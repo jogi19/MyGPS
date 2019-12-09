442187f5cbc574455aada137b0947d6104a03de8
Jochen Weiss <jochen.weiss@mail.de>
2019-12-09 21:33:18 +0100

    09-12-2019

--- app/src/main/java/jogi/de/mygps/MainActivity.java
+++ app/src/main/java/jogi/de/mygps/MainActivity.java
@@ -1,12 +1,58 @@
 package jogi.de.mygps;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+
 import android.os.Bundle;
+import 	android.content.pm.PackageManager;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 
 public class MainActivity extends AppCompatActivity{
+    private final static int MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1;
+    //final List<String> missingPermissions = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LocationListenerImpl ll = new LocationListenerImpl(this);
+        //requestPermissions(this, new String[] {
+        //               Manifest.permission.ACCESS_FINE_LOCATION,
+        //               },
+        //               );
+        //}
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ACCESS_FINE_LOCATION},
+                    MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
         }
-   }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+}
